c03d8658874a5d58f3945126f1b1ae415009f4a5
Tyler <tylertaydus4444@gmail.com>
2019-02-08 12:58:36 -1000

    stuck on check method

--- js/shopping-list-item.js
+++ js/shopping-list-item.js
@@ -1,6 +1,13 @@
 class ShoppingListItem{
     constructor(name, description){
       this.name = 'name';
-      this.description = 'description';
+      this.description = 'description'; 
+      this.isDone = 'isDone';
     }
+
+    check(){
+        this.isDone = 'true';
+    };
+
+
   };
--- test/shopping-list.test.js
+++ test/shopping-list.test.js
@@ -2,23 +2,42 @@ chai.should()
 const expect = chai.expect;
 const assert = chai.assert;
 
-describe('ShoppingListItem', () => {
+describe('ShoppingListItem is a Class with properties', () => {
   
   it('ShoppingListItem is a class', () => {
     expect(ShoppingListItem).to.be.a('function');
   });
   
   it('ShoppingListItem has a name property', () => {
-    let newObjectPassingName = new ShoppingListItem('name');
+    let newObjectPassingName = new ShoppingListItem('IceCream');
     newObjectPassingName.name.should.equal('name');
   });
 
+  it('ShoppingListItem has a isDone property', () => {
+    let newObjectPassingIsDone = new ShoppingListItem('IceCream');
+    newObjectPassingIsDone.isDone.should.equal('isDone');
+  })
+
   it('ShoppingListItem has a description property', () => {
-    let newObjectPassingDescription = new ShoppingListItem('description');
+    let newObjectPassingDescription = new ShoppingListItem('Chocolate');
     newObjectPassingDescription.description.should.equal('description');
   });
 
 });
 
+describe('ShoppingListItem has a check method', () => {
+  
+  it('Check is a function', () => {
+    let newObjectPassingCheck = new ShoppingListItem('check');
+    newObjectPassingCheck.check.should.be.a('function');
+  })
+
+  it('ShoppingListItem method check() will set isDone to true', () => {
+    ShoppingListItem.check;
+    assert.equal(ShoppingListItem.isDone, 'true');
+  })
+
+})
+  
 
 
